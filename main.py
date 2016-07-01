6c16415ba889d1143d536c9a71372f1b6cce71a9
Moisés Neto <mscn@cin.ufpe.br>
2016-07-01 20:08:54 -0300

    Merging and creating .gitattributes file

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://mscn:199132@localhost/urlshortener'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
 
