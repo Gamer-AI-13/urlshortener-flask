331adb13461a62f08f2afc602ce231d7dc2903f3
Moisés Neto <mscn@cin.ufpe.br>
2016-07-01 18:35:44 -0300

    Adding heroku changes

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from flask import Flask
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://mscn:199132@localhost/urlshortener'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://mscn:199132@localhost/urlshortener'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
 
