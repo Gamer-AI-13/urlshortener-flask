04024834998e8deabf3fb621926c2547256eb7d6
Gamer-AI-13 <67921075+Gamer-AI-13@users.noreply.github.com>
2021-11-19 00:30:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ import os
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://mscn:199132@localhost/urlshortener'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL", "")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
+app.secret_key = os.environ.get("SECRET_KEY", "")
 
 from views import *
 
