887d6f66a1b4b9faccc2c1eaea7f989d1c74a7ed
Gamer-AI-13 <67921075+Gamer-AI-13@users.noreply.github.com>
2021-11-19 23:46:16 +0530

    test

---
+++ database.py
@@ -0,0 +1,32 @@
+import datetime
+import pymongo
+
+class urlall:
+    
+    def __init__(self, uri, database_name):
+        self._client = pymongo.MongoClient(uri)
+        self.db = self._client[database_name]
+        self.col = self.db.users      
+    def new_url(self, shurl, url, views):
+        return dict(
+            shurl = shurl,
+            url = url,
+            views = views,
+            join_date = datetime.date.today().isoformat()
+        )  
+    async def add_url(self, shurl, url):
+         user = self.new_url(shurl, url, 0)
+         self.col.insert_one(user)
+    async def is_surl_exist(self, shurl):
+         user = self.col.find_one({'shurl': shurl})
+         print(user)
+         return True if user else False
+    #async def total_users_count(self):
+        #count = await self.col.count_documents({})
+        #return count
+    async def get_info(self, shurl):
+         user = self.col.find_one({'shurl': shurl})
+         print (user)
+         return user
+    async def delete_url(self, shurl):
+         self.col.delete_one({'shurl': shurl})
--- views.py
+++ views.py
@@ -1,7 +1,9 @@
 from flask import render_template, request, redirect, url_for
-from models import Url, db
 from main import app
 from datetime import datetime
+from database import urlall
+DATABASENAME = "urls"
+db = urlall(Config.DATABASE_URL, DATABASENAME)
 
 
 @app.route('/', methods=['GET'])
@@ -13,38 +15,41 @@ def shrink():
     # TODO parse the input
     new_url = request.form['url_input']
     print(new_url)
-
+    shorturl = ''.join([random.choice(string.ascii_lowercase + string.digits + string.ascii_uppercase) for n in range(5)])
+    print(shorturl)
     # First, we check if this URL isn't already shortened
-    _url = Url.query.filter_by(url = new_url).first()
-    if not _url:
+    surls = db.is_surl_exist(shorturl)
+    if not surls:
         # In case it doesn't exist
-        _url = Url(new_url, request.remote_addr)
-        db.session.add(_url)
-        db.session.commit()
+        surls = db.add_url(shorturl, request.remote_addr)
+        shortedurl = shorturl
     else:
         # If exists, just update the update date
-        _url.update_date = datetime.utcnow()
-        db.session.commit()
+        return redirect('/404')
 
-    return redirect('/info/{hash}'.format(hash = _url.url_hash))
+    return redirect('/info/{shortedurl}'.format(shortedurl = shortedurl))
 
 @app.route('/info/<url_hash>', methods=['POST', 'GET'])
 def info(url_hash):
-    _url = Url.query.filter_by(url_hash = url_hash).first()
+    _url = db.is_surl_exist(url_hash)
     if _url:
         domain = request.headers['host']
-        return render_template('info.html', url = _url, domain = domain)
+        url = db.get_info(url_hash)
+        ssurl = url['url']
+        return render_template('info.html', url = ssurl, domain = domain)
     else:
         return redirect('/404')
 
 @app.route('/<url_hash>')
 def page_redirect(url_hash):
-    _url = Url.query.filter_by(url_hash = url_hash).first()
+    _url = db.is_surl_exist(url_hash)
 
     if _url:
-        _url.click_count += 1
-        db.session.commit()
-        return redirect(_url.url)
+        #_url.click_count += 1
+        #db.session.commit()
+        url = db.get_info(url_hash)
+        urlin = url['url']
+        return redirect(urlin)
     else:
         return redirect('/404')
 
